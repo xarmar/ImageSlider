c8f6769e88bb48859293c7b11438936de2fce49e
Xavier Marques <xavierpmarques@gmail.com>
2021-07-09 20:47:38 +0100

    move most code to diffent files Js files

--- src/index.js
+++ src/index.js
@@ -1,64 +1,21 @@
 require('./styles.scss');
-import One from './images/1.jpg';
-import Two from './images/2.jpeg';
-import Three from './images/3.jpeg';
-import Four from './images/4.jpeg';
-import Five from './images/5.jpeg';
+import { carrouselControl, setLoopTo } from './carrouselControl';
+import { sleep } from './helperFunctions';
 
-// Identify Image Element
-const imageToChange = document.querySelector('#image');
+// Identify Left Arrow and Add event Listener
+const leftArrow = document.querySelector('#leftArrow');
+leftArrow.addEventListener('click', function () {
+  carrouselControl.previousImageInCarrousel(true);
+  sleep(5000);
+  setLoopTo(true);
+});
 
-// Create Array of Images
-const arrayOfImagesThatWillCycle = [One, Two, Three, Four, Five];
+// Identify Right Arrow and Add event Listener
+const rightArrow = document.querySelector('#rightArrow');
+rightArrow.addEventListener('click', function () {
+  carrouselControl.nextImageInCarrousel(true);
+  sleep(5000);
+  setLoopTo(true);
+});
 
-let imageLink = arrayOfImagesThatWillCycle[0];
-imageToChange.setAttribute('src', imageLink);
-
-const sleep = ms => {
-  return new Promise(resolve => setTimeout(resolve, ms));
-};
-
-const nextImage = () => {
-  // Find out what image is displayed right now
-  let currentIndex = arrayOfImagesThatWillCycle.indexOf(imageLink);
-  let nextIndex = currentIndex + 1;
-
-  // If nextIndex goes over the array length, reset imageLink to be the first element
-  if (nextIndex > arrayOfImagesThatWillCycle.length - 1) {
-    imageLink = arrayOfImagesThatWillCycle[0];
-  }
-  // else, set imageLink to be nextIndex
-  else {
-    imageLink = arrayOfImagesThatWillCycle[nextIndex];
-  }
-  imageToChange.setAttribute('src', imageLink);
-};
-
-// const previousImage = () => {
-//   // Find out what image is displayed right now
-//   let currentIndex = arrayOfImagesThatWillCycle.indexOf(imageLink);
-//   let previousIndex = currentIndex - 1;
-
-//   // If previousIndex is < 0, set imageLink to be the last element
-//   if (previousIndex < 0) {
-//     imageLink = arrayOfImagesThatWillCycle[arrayOfImagesThatWillCycle.length];
-//   }
-//   // else, set imageLink to be previousIndex
-//   else {
-//     imageLink = arrayOfImagesThatWillCycle[previousIndex];
-//   }
-// };
-
-const changeImages = async () => {
-  for (let i = 0; i < 20; i++) {
-    nextImage();
-    imageToChange.classList.toggle('easeIn');
-    await sleep(4000);
-    imageToChange.classList.toggle('easeIn');
-    imageToChange.classList.toggle('easeOut');
-    await sleep(1000);
-    imageToChange.classList.toggle('easeOut');
-  }
-};
-
-changeImages();
+carrouselControl.autoScrollLoop();
