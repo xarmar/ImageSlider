10ecac7d755e4025143cb7725b20ea3ecf5f53a8
Xavier Marques <xavierpmarques@gmail.com>
2021-07-10 00:39:34 +0100

    remove unnecessary imports and unnecessary code

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
 require('./styles.scss');
-import { carrouselControl, setLoopTo } from './carrouselControl';
-import { sleep } from './helperFunctions';
+import { carrouselControl } from './carrouselControl';
 
 // Identify Left Arrow and Add event Listener
 const leftArrow = document.querySelector('#leftArrow');
 leftArrow.addEventListener('click', function () {
-  carrouselControl.previousImageInCarrousel(true);
-  sleep(5000);
-  setLoopTo(true);
+  carrouselControl.previousImageInCarrousel();
 });
 
 // Identify Right Arrow and Add event Listener
 const rightArrow = document.querySelector('#rightArrow');
 rightArrow.addEventListener('click', function () {
-  carrouselControl.nextImageInCarrousel(true);
-  sleep(5000);
-  setLoopTo(true);
+  carrouselControl.nextImageInCarrousel();
 });
 
 carrouselControl.autoScrollLoop();
